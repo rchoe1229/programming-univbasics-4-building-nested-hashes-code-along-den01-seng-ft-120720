10f60b0881e15f7e014f368088b79da5971e175d
Ryan Choe <ryan.choe@outlook.com>
2020-11-25 23:04:04 +0000

    Automatically backed up by Learn

--- lib/01_first_challenge.rb
+++ lib/01_first_challenge.rb
@@ -4,8 +4,10 @@ def first_challenge
        :patriarch => {:name => "Lord Montague", :age => "53"}, 
      :matriarch => {:name => "Lady Motague", :age => "54"}, 
      :hero => {:name => "Romeo",:age => "15"},
-     :hero_friends => [{:name => "Benvolio", :age => "17", :attitude => "worried"},
-       {:name => "Mercutio", :age => "18", :attitude => "hot-headed"}]
+     :hero_friends => [
+       {:name => "Benvolio", :age => "17", :attitude => "worried"},
+       {:name => "Mercutio", :age => "18", :attitude => "hot-headed"}
+       ]
      },
      :capulet => {
        :patriarch => {:name => "Lord Capulet", :age => "50"}, 
