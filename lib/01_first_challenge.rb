a447bdb749935acb944f65235fee46769ff022bf
Ryan Choe <ryan.choe@outlook.com>
2020-11-25 23:13:45 +0000

    Automatically backed up by Learn

--- lib/01_first_challenge.rb
+++ lib/01_first_challenge.rb
@@ -1,9 +1,7 @@
 def first_challenge
    epic_tragedy = {
      :montague => {
-       :patriarch => {:name => "Lord Montague", :age => "53"}, 
-     :matriarch => {:name => "Lady Montague", :age => "54"}, 
-     :hero => {:name => "Romeo",:age => "15"},
+      
      :hero_friends => [
        
        ]
