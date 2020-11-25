2c02c86da4ee5b4ac94040aceddc26f6186c882e
Ryan Choe <ryan.choe@outlook.com>
2020-11-25 22:49:36 +0000

    Automatically backed up by Learn

--- lib/01_first_challenge.rb
+++ lib/01_first_challenge.rb
@@ -15,7 +15,7 @@ def first_challenge
      :hero_friends => [{
        :name => "Benvolio", :age => "17", :attitude => "worried" 
      },
-       :name => "Mercutio", :age => "18", :attitude => "hot-headed"
+       {:name => "Mercutio", :age => "18", :attitude => "hot-headed"}
      ]
      },
      :capulet => {
